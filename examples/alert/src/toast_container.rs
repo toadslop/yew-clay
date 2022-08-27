4d124ca38ad5d611edcb0fd4ee58afffd6721c9f
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-08-27 09:52:42 +0900

    all toast items now disappear as they should

--- examples/alert/src/toast_container.rs
+++ examples/alert/src/toast_container.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{CONTAINER_CLASS, SPRITEMAP};
 use yew::{html, Component, Context, Html};
 use yew_clay::ClayContainer;
@@ -5,11 +7,12 @@ use yew_clay::ClayToastContainer;
 use yew_clay::{AutoCloseValue, ClayAlert};
 
 pub struct WithToastContainer {
-    toast_items: Vec<f64>,
+    toast_items: HashMap<usize, f64>,
+    next_idx: usize,
 }
 
 pub enum Msg {
-    AddItem,
+    AddItem(usize),
     RemoveItem(usize),
 }
 
@@ -19,38 +22,46 @@ impl Component for WithToastContainer {
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            toast_items: Vec::new(),
+            toast_items: HashMap::new(),
+            next_idx: 0,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddItem => {
-                self.toast_items.push(js_sys::Math::random());
+            Msg::AddItem(idx) => {
+                self.toast_items.insert(idx, js_sys::Math::random());
+                self.next_idx = self.next_idx + 1;
                 true
             }
             Msg::RemoveItem(index) => {
-                self.toast_items.remove(index);
+                gloo_console::log!(format!("{:?}", self.toast_items));
+                self.toast_items.remove(&index);
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let next_idx = self.next_idx;
         html! {
             <ClayContainer class={CONTAINER_CLASS}>
                 <h2>{"Using with ToastContainer"}</h2>
                 <div>
-                    <button onclick={ctx.link().callback(|_| Msg::AddItem)}>
+                    <button onclick={ctx.link().callback(move |_| Msg::AddItem(next_idx))}>
                         {"Add Alert"}
                     </button>
                 </div>
                 <ClayToastContainer>
-                    {self.toast_items.iter().enumerate().map(|(index, value)| {html!{
+                    {self.toast_items.iter().map(|(index, value)| {
+                        let idx = *index;
+                        html!{
                         <ClayAlert
                             auto_close={AutoCloseValue::Number(5000)}
-                            key={index}
-                            on_close={ctx.link().callback(move |_| Msg::RemoveItem(index))}
+                            key={idx}
+                            on_close={ctx.link().callback(move |_| {
+
+                                Msg::RemoveItem(idx)})}
                             spritemap={SPRITEMAP}
                             title={"Hola:"}
                             >
